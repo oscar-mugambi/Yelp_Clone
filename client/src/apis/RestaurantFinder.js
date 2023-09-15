de108b9055ce621ad10b22fc59f3718d118f2329
oscar mugambi <oscarmugambi@gmail.com>
2023-09-16 02:18:58 +0300

    minor fixes before deployment

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -1,5 +1,10 @@
-import axios from "axios"
+import axios from 'axios';
+
+const baseURL =
+  process.env.NODE_ENV === 'production'
+    ? '/api/v1/restaurants'
+    : 'http://localhost:3001/api/v1/restaurants';
 
 export default axios.create({
-  baseURL: "http://localhost:3000/api/v1/restaurants",
-})
+  baseURL,
+});
